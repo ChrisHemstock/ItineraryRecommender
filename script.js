075aff8e7c7880047d0df6b54f0e096f38e49d1d
ChrisHemstock <chris.hemstock@myemail.indwes.edu>
2022-10-03 22:16:51 -0400

    worked on fetching nodes from json

--- script.js
+++ script.js
@@ -1,12 +1,32 @@
-var map = L.map('map').setView([51.505, -0.09], 13);
+var map = L.map('map').setView([39.80924029431849, -86.16061656273943], 13);
+
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '© OpenStreetMap'
 }).addTo(map);
 
-L.marker([51.5, -0.09]).on('click', function(e) {
+let markerArray = []
+markerArray.push(
+    L.marker([39.80924029431849, -86.16061656273943]).on('click', function(e) {
     var newElement = document.createElement("p");
     newElement.innerHTML = e.latlng;
     document.getElementById("poiList").appendChild(newElement);
-}).addTo(map)
+    })
+)
+
+fetch("nodes.json")
+    .then(response => response.json())
+    .then(data => {
+        data.features.forEach(element => {
+            L.marker(element.geometry.coordinates).on('click', function(e) {
+                var newElement = document.createElement("p");
+                newElement.innerHTML = e.latlng;
+                document.getElementById("poiList").appendChild(newElement);
+                })
+        });
+    })
+
+for(let i=0; i<markerArray.length; i++) {
+    markerArray[i].addTo(map)
+}
