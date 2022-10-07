cda70d1d9883d5b252cf2d65f2bce2de8a0e1243
ChrisHemstock <chris.hemstock@myemail.indwes.edu>
2022-10-07 16:38:32 -0400

    allows only one of the same location in a list. Remove list elements.

--- script.js
+++ script.js
@@ -12,11 +12,28 @@ fetch("nodes.json")
     .then(data => {
         data.elements.forEach(element => {
             L.marker([element.lat, element.lon]).on('click', function(e) {
-                let newElement = document.createElement("p");
-                newElement.innerHTML = element.tags.name;
-                document.getElementById("poiList").appendChild(newElement);
-                }).addTo(map)
+                //only adds list element if an element with that id doesn't exist
+                if(!document.getElementById(element.id)) {
+                    //adds a list element to the side
+                    var html = '<li id="' + element.id + '">' + element.tags.name + '<span class="close">X</span></li>';
+                    document.getElementById('poiList').insertAdjacentHTML('beforeend', html);
+
+                    //allows buttons to be closed
+                    let closebtns = document.getElementsByClassName("close");
+                    for (let i = 0; i < closebtns.length; i++) {
+                        closebtns[i].addEventListener("click", function() {
+                            this.parentElement.remove()
+                        });
+                    }
+                }
+            }).bindPopup(element.tags.name).on('mouseover', function (e) {
+                this.openPopup();
+            }).on('mouseout', function (e) {
+                this.closePopup();
+            }).addTo(map)
         });
         
     })
-//https://www.w3schools.com/howto/howto_js_close_list_items.asp
+
+
+    
