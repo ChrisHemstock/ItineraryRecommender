bb0d590bd221134728697f4337f2999600d22761
Alec <alecjoshuagreen@gmail.com>
2022-10-18 23:19:38 -0400

    commit on 'Alec-Branch'

---
+++ TestFetch.php
@@ -0,0 +1,27 @@
+<?php
+
+require_once "dbconnect.php";
+
+
+$results= $link->query('SELECT * FROM POIs;')->fetch_all();
+
+$data = array();
+foreach ($results as $row) {
+    $Lat = $row[0];
+    $Lng = $row[1];
+    $Category = $row[2];
+    $id = $row[3];
+    $address = $row[4];
+    $phone = $row;
+    $name = $row;
+    $rating = $row[7];
+    $num_ratings = $row[8];
+    $data[] = array($Lat, $Lng, $Category, $id, $address, $phone, $name, $rating, $num_ratings);
+}
+ $json = json_encode(array("data" => $data));
+
+echo $json;
+
+return $json;
+
+?>
