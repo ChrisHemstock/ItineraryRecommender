54fbead33ac3096211d50f947791580738b56e31
ChrisHemstock <chris.hemstock@myemail.indwes.edu>
2022-10-19 20:22:02 -0400

    default start and end times are set for events

--- script.js
+++ script.js
@@ -54,8 +54,10 @@ function getDragAfterElement(container, y) {
 function addEvent(element) {
     // if(!document.getElementById(element.id)) {
     //     let html = '<li id="' + element.id + '" class="draggable" draggable="true">' + element.tags.name + '<span class="close">X</span></li>';
+        
         let html = '<li class="draggable" draggable="true">' + element.tags.name + 
-                    '<span class="time"><input type="time" class="startEvent" title="Start Time"/><input type="time" class="endEvent" title="End Time"/></span>' +
+                    '<span class="time"><input type="time" class="startEvent" title="Start Time" value="'+ getStartTime() + 
+                    '"/><input type="time" class="endEvent" title="End Time" value="' + incrementTime(getStartTime(), 30) + '"/></span>' +
                     '<span class="close">X</span></li>';
         let poi = document.getElementById('poi')
         poi.insertAdjacentHTML('beforeend', html);
@@ -63,6 +65,29 @@ function addEvent(element) {
         let newElement = [...document.querySelectorAll('.draggable:not(.dragging)')].pop()
         addEventEventListeners(newElement)
     // }
+
+    function getStartTime() {
+        if([...document.querySelectorAll('.draggable:not(.dragging)')].length == 0) {
+            return "00:00"
+        } else {
+            return [...document.querySelectorAll('.draggable:not(.dragging)')].pop().querySelector('.endEvent').value
+        }
+    }
+
+    function incrementTime(time, minutesAdded) {
+        let timeArray = time.split(':')
+        timeArray[0] = Number(timeArray[0])
+        timeArray[1] = Number(timeArray[1]) + minutesAdded
+        console.log(timeArray)
+        while(timeArray[1] >= 60) {
+            timeArray[1] = timeArray[1] - 60;
+            timeArray[0] += 1;
+            if(timeArray[0] > 23) {
+                timeArray[0] = 0
+            }
+        }
+        return String(timeArray[0]).padStart(2, '0') + ':' + String(timeArray[1]).padStart(2, '0')
+    }
 }
 
 function addEventEventListeners(element) {
