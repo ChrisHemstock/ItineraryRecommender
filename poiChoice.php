caae885853bba9c370fae95665a354010efdb21e
ChrisHemstock <chris.hemstock@myemail.indwes.edu>
2023-01-22 15:25:15 -0500

    the liked id's are ready to be inserted into the database

--- poiChoice.php
+++ poiChoice.php
@@ -16,7 +16,7 @@
     //Takes an array of POI IDs and a position and echos the html for that id
     //When the button is clicked the next poi is echoed
     //Also removes all markup within the div #choice
-    function getPoiInfo($link, $ids, $pos) {
+    function getPoiInfo($link, $ids, $pos, $likes) {
         $id = $ids[$pos];
         $results = $link->query('SELECT name FROM pois WHERE id = ' . $id . ';')->fetch_all();
 
@@ -28,16 +28,12 @@
             echo '<div id="' . $id . '">
                     <h1 class=' . $id . '>' . $name . '</h1>
                     <form method="post">
-                        <button type="submit" name="Like" value="' . $pos . '">Like</button>
-                        <button type="submit" name="Dislike" value="' . $pos . '">Dislike</button>
+                        <button type="submit" name="Like' . $pos . '" value="' . $likes . ' ' . $id . '">Like</button>
+                        <button type="submit" name="Dislike' . $pos . '" value="' . $likes . '">Dislike</button>
                         <img src="" alt="">
                     </form>
                   </div>';
         }
-        if(isset($_POST['Like' . $id]) or isset($_POST['Dislike' . $id])) {
-            echo $id . '<br>';
-            
-        } 
     }
 ?>
 
@@ -55,18 +51,41 @@
     <div id="choice">
     <?php
         $ids = [1119, 1198, 1028, 1062];
+        $likes = '';
         $start = true;
-        for ($i = 0; $i < sizeof($ids); $i++) {
+        for ($i = 0; $i < sizeof($ids) - 1; $i++) {
             echo 'here:' . $i . '<br>';
             if(isset($_POST['Like' . $i]) or isset($_POST['Dislike' . $i])) {
                 $start = false;
-                getPoiInfo($link, $ids, $i + 1);
+                if(isset($_POST['Like' . $i])) {
+                    $likes = $_POST['Like' . $i];
+                } else {
+                    $likes = $_POST['Dislike' . $i];
+                }
+                getPoiInfo($link, $ids, $i + 1, $likes);
                 break;
             }
+            
+        }
+        if(isset($_POST['Like' . sizeof($ids) - 1]) or isset($_POST['Dislike' . sizeof($ids) - 1])) {
+            $start = false;
+            if(isset($_POST['Like' . $i])) {
+                $likes = $_POST['Like' . $i];
+            } else {
+                $likes = $_POST['Dislike' . $i];
+            }
+            //Insert the id's in $likes into the database here
+            //Likes is a string of id's seperated by spaces
+
+
+
+
+            header("Location: account.php");
+            exit();
         }
         if($start) {
             echo 'here:-1<br>';
-            getPoiInfo($link, $ids, 0); 
+            getPoiInfo($link, $ids, 0, $likes); 
         }
     ?>
     
