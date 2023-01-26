25c6c9bfa74de07709d3e48d6191c17768a86928
ChrisHemstock <chris.hemstock@myemail.indwes.edu>
2023-01-26 00:21:28 -0500

    Finished recommendations

--- resources/reviewRequest.php
+++ resources/reviewRequest.php
@@ -14,38 +14,10 @@ set_time_limit(360);
 //get user interests
 function userInterests($userID) {
   global $link;
-  $interests = $link->query('SELECT Description FROM interests WHERE userID = ' . $userID . ' ;')->fetch_all();
+  $poiReviews = $link->query('SELECT reviews FROM likes, pois WHERE userID = ' . $userID . ' AND pois.API_ID = likes.POI_ID ;')->fetch_all();
   $likes = array();
-  foreach($interests as $interest) {
-    $txt = '';
-    if($interest[0] == 'artsEntertainment') {
-      $txt = 'art';
-    } else if($interest[0] == 'beautyFitness') {
-      $txt = 'fitness';
-    } else if($interest[0] == 'books') {
-      $txt = 'book';
-    } else if($interest[0] == 'businessIndustrial') {
-      $txt = 'industrial';
-    } else if($interest[0] == 'electronics') {
-      $txt = 'electronic';
-    } else if($interest[0] == 'finance') {
-      $txt = 'finance';
-    } else if($interest[0] == 'food') {
-      $txt = 'food';
-    } else if($interest[0] == 'games') {
-      $txt = 'game';
-    } else if($interest[0] == 'homeGarden') {
-      $txt = 'garden';
-    } else if($interest[0] == 'internetTelecom') {
-      $txt = 'communicate';
-    } else if($interest[0] == 'jobsEducation') {
-      $txt = 'educate';
-    } else if($interest[0] == 'leisure') {
-      $txt = 'leisure';
-    } else if($interest[0] == 'vehicles') {
-      $txt = 'vehicle';
-    }
-    array_push($likes, $txt);
+  foreach($poiReviews as $poiReview) {
+    array_push($likes, $poiReview);
   }
   return $likes;
 }
@@ -72,7 +44,7 @@ function getReviewsArray() {
     $review = '';
 
     foreach($data["reviews"] as $row) {
-      $text = strtolower(preg_replace('/[^A-Za-z0-9\- ]/', '', $row['text']));
+      $text = strtolower(preg_replace('/[^A-Za-z\ ]/', '', $row['text']));
       $review .= ' ' . $text;
     }
 
@@ -127,93 +99,143 @@ function poiRecommendArray($docs, $likes, $tfidf, $userProfile) {
 
 
 function getRecommendations($link, $userID) {
-  $interests = $link->query('SELECT Description FROM interests WHERE userID = ' . $userID . ' ;')->fetch_all();
-  if(count($interests) > 0) {
+
+  //
+  // Get all the tokens for the likes in one document
+  //
+  //
+  $poiReviews = $link->query('SELECT reviews FROM likes, pois WHERE userID = ' . $userID . ' AND pois.API_ID = likes.POI_ID ;')->fetch_all();
+  if(count($poiReviews) > 0) {
     $likes = array();
-    foreach($interests as $interest) {
-      $txt = '';
-      if($interest[0] == 'artsEntertainment') {
-        $txt = 'art';
-      } else if($interest[0] == 'beautyFitness') {
-        $txt = 'fitness';
-      } else if($interest[0] == 'books') {
-        $txt = 'book';
-      } else if($interest[0] == 'businessIndustrial') {
-        $txt = 'industrial';
-      } else if($interest[0] == 'electronics') {
-        $txt = 'electronic';
-      } else if($interest[0] == 'finance') {
-        $txt = 'finance';
-      } else if($interest[0] == 'food') {
-        $txt = 'food';
-      } else if($interest[0] == 'games') {
-        $txt = 'game';
-      } else if($interest[0] == 'homeGarden') {
-        $txt = 'garden';
-      } else if($interest[0] == 'internetTelecom') {
-        $txt = 'communicate';
-      } else if($interest[0] == 'jobsEducation') {
-        $txt = 'educate';
-      } else if($interest[0] == 'leisure') {
-        $txt = 'leisure';
-      } else if($interest[0] == 'vehicles') {
-        $txt = 'vehicle';
-      }
-      array_push($likes, $txt);
+    foreach($poiReviews as $poiReview) {      
+      $review = explode(' ', strtolower(preg_replace('/[^A-Za-z\ ]/', '', $poiReview[0])));
+      array_shift($review);
+
+
+      $likes = array_merge($likes, $review);
     }
+    //var_dump($likes);
 
-
-    //Get reviews      returns an array of reviews
     $docs = [];
-    $tokens = new TokensDocument(tokenize('art fitness book industrial electronic finance food game garden communicate educate leisure vehicle'));
-    $docs['interests'] = $tokens;
+    $tokens = new TokensDocument($likes);
+    $docs['likes'] = $tokens;
+    //var_dump($docs['likes']);
+    
+
+
+    //
+    // Get all review tokens in one document
+    //
+    //
+    $POI_reviews = $link->query('SELECT reviews, id FROM POIs')->fetch_all();
+    $allReviews = array();
+    foreach($POI_reviews as $poiReview) {      
+      $review = explode(' ', strtolower(preg_replace('/[^A-Za-z\ ]/', '', $poiReview[0])));
+      array_shift($review);
+      
+
+
+      $allReviews = array_merge($allReviews, $review);
+    }
+    
+    $allReviews = array_unique($allReviews);
+    $allReviewTokens = new TokensDocument($allReviews);
+    $docs['all'] = $allReviewTokens;
+    //var_dump($docs['all']);
+    //
+    // Get all review tokens in one document per poi
+    //
+    //
+
 
 
     $POI_reviews = $link->query('SELECT reviews, id FROM POIs')->fetch_all();
+    //$poiDocs = array();
     foreach ($POI_reviews as $review) {
-      $tokens = new TokensDocument(tokenize($review[0]));
-      $docs[$review[1]] = $tokens;
       //var_dump($review);
+      if($review[0] != Null) {
+        $tokens = new TokensDocument(tokenize($review[0]));
+        $docs[$review[1]] = $tokens;
+      }
     }
+    //var_dump($docs);
 
 
 
+    //var_dump($likes);
+    //var_dump($poiDocs);
+    
+    //var_dump($docs['likes']);
 
+
+
+    //
+    // TFIDF
+    //
+    //
     $docsCollection = new DocumentArrayCollection($docs);
-    $tfidf = new TfIdf($docsCollection);
+    $userTfidf = new TfIdf($docsCollection);
 
 
     //creates the user profile vector
+
     $userProfile = [];
-    foreach($likes as $like) {
-      array_push($userProfile, $tfidf->getTfIdf($docs['interests'], $like, 3));
+    foreach($allReviews as $word) {
+      if($word != '') {
+        array_push($userProfile, $userTfidf->getTfIdf($docs['likes'], $word, 3));
+      }
     }
+    //var_dump($userProfile);
 
 
-    //returns an ordered list of POIs to recommend
+    $POI_reviews = $link->query('SELECT reviews, id FROM POIs')->fetch_all();
     $poisLiked = [];
-    foreach($docs as $key=>$poi) {
-      $vector = [];
-      foreach($likes as $like) {
-        array_push($vector, $tfidf->getTfIdf($poi, $like, 3));
-      }
-      $similarity = cosineSimilarity($userProfile, $vector);
-      if($similarity != -2) {
-        $poisLiked[$key] = $similarity;
-      }
+    foreach($POI_reviews as $poiReview) { 
+      if($poiReview[0] != '') {
+        // $review = explode(' ', strtolower(preg_replace('/[^A-Za-z\ ]/', '', $poiReview[0])));
+        // //array_shift($review);
+        // $review = array_filter($review);
+        // $review
+        $vector = [];
+          //var_dump($token);
+        foreach($allReviews as $word) {
+          if($word != '') {
+            array_push($vector, $userTfidf->getTfIdf($docs[$poiReview[1]], $word, 3));
+          }
+        }
+                                              //review, each word in all
+        
+        // var_dump(count($vector));
+        // var_dump(count($userProfile));
+        
+        $similarity = cosineSimilarity($userProfile, $vector);
+        //var_dump($similarity);
+        if($similarity != -2) {
+          $poisLiked[$poiReview[1]] = $similarity;
+        }
+        
+      } 
     }
     asort($poisLiked);
     array_pop($poisLiked);
-    return json_encode(array_slice(array_reverse($poisLiked, true), 0, 10, true));
-
-
-
+    //var_dump($docs['all']);
     // //Prints the recommended POIs
     // foreach($poisLiked as $key=>$value) {
     //   echo $key . '=' . $value . '<br>';
     // }
+
+    //returns an ordered list of POIs to recommend
+    
+    
+    return json_encode(array_slice(array_reverse($poisLiked, true), 0, 5, true));
+    
+
+
+
+    
   }
 }
+
 
 
 
