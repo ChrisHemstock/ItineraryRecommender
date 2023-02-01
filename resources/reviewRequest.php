b363983390e21c6b90674ff25901e52be5456390
Alec <alecjoshuagreen@gmail.com>
2023-01-31 20:02:09 -0500

    Update RR

--- resources/reviewRequest.php
+++ resources/reviewRequest.php
@@ -8,95 +8,6 @@ require_once 'vendor/autoload.php';
 
 require_once "includes/dbconnect.php";
 set_time_limit(360);
-//$userID = $_SESSION["id"];
-
-
-// //get user interests
-// function userInterests($userID) {
-//   global $link;
-//   $poiReviews = $link->query('SELECT reviews FROM likes, pois WHERE userID = ' . $userID . ' AND pois.API_ID = likes.POI_ID ;')->fetch_all();
-//   $likes = array();
-//   foreach($poiReviews as $poiReview) {
-//     array_push($likes, $poiReview);
-//   }
-//   return $likes;
-// }
-
-// function getReviewsArray() {
-//   global $link;
-//   $docs = [];
-//   $tokens = new TokensDocument(tokenize('art fitness book industrial electronic finance food game garden communicate educate leisure vehicle'));
-//   $docs['interests'] = $tokens;
-
-//   $client = new \GuzzleHttp\Client();
-
-//   $api_id = $link->query('SELECT id, API_ID FROM POIs')->fetch_all();
-//   foreach ($api_id as $id) {
-//     sleep(1);
-//     $response = $client->request('GET', 'https://api.yelp.com/v3/businesses/' . $id[1] . '/reviews?limit=20&sort_by=yelp_sort', [
-//       'headers' => [
-//         'Authorization' => 'Bearer FPHBQC5fbtVpUqt4lQtAmTPXNWzDKblHryRIRIfoL5PYHgLmW109muvBkAqYyscdeNerih_ZQrxs4WGnp-xf4pgyBDbEmO36NlUS8MB6GvgJp52qoqW_nUdvG9uOY3Yx',
-//         'accept' => 'application/json',
-//       ],
-//     ]);
-//     $data = json_decode($response->getBody(), true);
-
-//     $review = '';
-
-//     foreach($data["reviews"] as $row) {
-//       $text = strtolower(preg_replace('/[^A-Za-z\ ]/', '', $row['text']));
-//       $review .= ' ' . $text;
-//     }
-
-//     //push review to database here ********** this could be up to three reviews concat. together
-
-//     $tokens = new TokensDocument(tokenize($review));
-//     $docs[$id[0]] = $tokens;
-//   }
-
-//   return $docs;
-// }
-
-// //  $likes = the array returned from userInterests
-// //  $tfidf = a tfidf object
-// //  $docs = the array from getReviewsArray
-// function createUserVector($likes, $tfidf, $docs) {
-//   $userProfile = [];
-//   foreach($likes as $like) {
-//     array_push($userProfile, $tfidf->getTfIdf($docs['interests'], $like, 3));
-//   }
-
-//   return $userProfile;
-// }
-
-// //  $docs = the array from getReviewsArray
-// //  $likes = the array returned from userInterests
-// //  $tfidf = a tfidf object
-// //  $userProfile = What is returned from create UserVector
-// function poiRecommendArray($docs, $likes, $tfidf, $userProfile) {
-//   $poisLiked = [];
-//   foreach($docs as $key=>$poi) {
-//     $vector = [];
-//     foreach($likes as $like) {
-//       array_push($vector, $tfidf->getTfIdf($poi, $like, 3));
-//     }
-//     $similarity = cosineSimilarity($userProfile, $vector);
-//     if($similarity != -2) {
-//       $poisLiked[$key] = $similarity;
-//     }
-//   }
-//   asort($poisLiked);
-//   array_pop($poisLiked);
-//   $poisLiked = array_reverse($poisLiked, true);
-// }
-
-
-
-
-//Functions above are for use later
-
-
-
 
 function getRecommendations($link, $userID) {
 
@@ -132,16 +43,13 @@ function getRecommendations($link, $userID) {
     foreach($POI_reviews as $poiReview) {      
       $review = explode(' ', strtolower(preg_replace('/[^A-Za-z\ ]/', '', $poiReview[0])));
       array_shift($review);
-      
-
-
       $allReviews = array_merge($allReviews, $review);
     }
     
     $allReviews = array_unique($allReviews);
     $allReviewTokens = new TokensDocument($allReviews);
     $docs['all'] = $allReviewTokens;
-    //var_dump($docs['all']);
+    //var_dump($allReviews);
     //
     // Get all review tokens in one document per poi
     //
@@ -182,9 +90,11 @@ function getRecommendations($link, $userID) {
     $userProfile = [];
     foreach($allReviews as $word) {
       if($word != '') {
+        //echo $word . ': ' . $userTfidf->getTfIdf($docs['likes'], $word, 3) . '<br>';
         array_push($userProfile, $userTfidf->getTfIdf($docs['likes'], $word, 3));
       }
     }
+    //var_dump($allReviews);
     //var_dump($userProfile);
 
 
@@ -205,8 +115,7 @@ function getRecommendations($link, $userID) {
         }
                                               //review, each word in all
         
-        // var_dump(count($vector));
-        // var_dump(count($userProfile));
+
         
         $similarity = cosineSimilarity($userProfile, $vector);
         //var_dump($similarity);
@@ -218,11 +127,8 @@ function getRecommendations($link, $userID) {
     }
     asort($poisLiked);
     array_pop($poisLiked);
-    //var_dump($docs['all']);
-    // //Prints the recommended POIs
-    // foreach($poisLiked as $key=>$value) {
-    //   echo $key . '=' . $value . '<br>';
-    // }
+
+   // var_dump($poisLiked);
 
     //returns an ordered list of POIs to recommend
     
@@ -236,57 +142,5 @@ function getRecommendations($link, $userID) {
   }
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// $response = $client->request('GET', 'https://api.yelp.com/v3/businesses/UFCN0bYdHroPKu6KV5CJqg/reviews?limit=20&sort_by=yelp_sort', [
-//   'headers' => [
-//     'Authorization' => 'Bearer FPHBQC5fbtVpUqt4lQtAmTPXNWzDKblHryRIRIfoL5PYHgLmW109muvBkAqYyscdeNerih_ZQrxs4WGnp-xf4pgyBDbEmO36NlUS8MB6GvgJp52qoqW_nUdvG9uOY3Yx',
-//     'accept' => 'application/json',
-//   ],
-// ]);
-
-// //echo $response->getBody();
-// //$data = $response->getBody();
-// $data = json_decode($response->getBody(), true);
-
-
-// $docs = [];
-// foreach($data["reviews"] as $row) {
-//   $text = strtolower(preg_replace('/[^A-Za-z0-9\- ]/', '', $row['text']));
-//   $tokens = new TokensDocument(tokenize($text));
-//   //$tokens->applyStemmer(new DictionaryStemmer(new EnchantAdapter(), new SnowballStemmer()));
-//   array_push($docs, $tokens);
-//   foreach($tokens->getDocumentData() as $token) {
-//     echo $token . '<br>';
-//   }
-// }
-// $docsCollection = new DocumentArrayCollection($docs);
-// $tfidf = new TfIdf($docsCollection);
-
-// //$chicken = $tfidf->getTfIdf($docs[1], 'Eagle', 3);
-// $chicken = $tfidf->getIdf();
-// foreach($chicken as $chick) {
-//   echo '<br>' . $chick;
-// }
-
-
-// foreach ($data["reviews"] as $row) {
-//   //get the POI details
-//   $review = $row['text'];
-//   echo '<br>' . $review;
-// }
 
 ?>
