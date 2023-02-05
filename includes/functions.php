f5edf55281992bcc7255f9fa8ca80489e9da2bf1
ChrisHemstock <chris.hemstock@myemail.indwes.edu>
2023-02-05 00:36:22 -0500

    US13 - Fixed reccomendations

--- includes/functions.php
+++ includes/functions.php
@@ -45,11 +45,19 @@ function cosineSimilarity($list1, $list2) {
     $dotSum = 0;
     $mag1 = 0;
     $mag2 = 0;
-    for($i = 0; $i < count($list1); $i++) {
-            $dotSum += $list1[$i] * $list2[$i];
-            $mag1 += $list1[$i] ** 2;
-            $mag2 += $list2[$i] ** 2;
+    // for($i = 0; $i < count($list1); $i++) {
+    //         $dotSum += $list1[$i] * $list2[$i];
+    //         $mag1 += $list1[$i] ** 2;
+    //         $mag2 += $list2[$i] ** 2;
+    // }
+
+    foreach($list1 as $word => $value) {
+        $dotSum += $list1[$word] * $list2[$word];
+        $mag1 += $list1[$word] ** 2;
+        $mag2 += $list2[$word] ** 2;
     }
+
+
 
     if($mag1 == 0 || $mag2 == 0) {
         return -2;
--- resources/reviewRequest.php
+++ resources/reviewRequest.php
@@ -10,93 +10,8 @@ require_once "includes/dbconnect.php";
 set_time_limit(360);
 //$userID = $_SESSION["id"];
 
-
-//get user interests
-function userInterests($userID) {
-  global $link;
-  $poiReviews = $link->query('SELECT reviews FROM likes, pois WHERE userID = ' . $userID . ' AND pois.API_ID = likes.POI_ID ;')->fetch_all();
-  $likes = array();
-  foreach($poiReviews as $poiReview) {
-    array_push($likes, $poiReview);
-  }
-  return $likes;
-}
-
-function getReviewsArray() {
-  global $link;
-  $docs = [];
-  $tokens = new TokensDocument(tokenize('art fitness book industrial electronic finance food game garden communicate educate leisure vehicle'));
-  $docs['interests'] = $tokens;
-
-  $client = new \GuzzleHttp\Client();
-
-  $api_id = $link->query('SELECT id, API_ID FROM POIs')->fetch_all();
-  foreach ($api_id as $id) {
-    sleep(1);
-    $response = $client->request('GET', 'https://api.yelp.com/v3/businesses/' . $id[1] . '/reviews?limit=20&sort_by=yelp_sort', [
-      'headers' => [
-        'Authorization' => 'Bearer FPHBQC5fbtVpUqt4lQtAmTPXNWzDKblHryRIRIfoL5PYHgLmW109muvBkAqYyscdeNerih_ZQrxs4WGnp-xf4pgyBDbEmO36NlUS8MB6GvgJp52qoqW_nUdvG9uOY3Yx',
-        'accept' => 'application/json',
-      ],
-    ]);
-    $data = json_decode($response->getBody(), true);
-
-    $review = '';
-
-    foreach($data["reviews"] as $row) {
-      $text = strtolower(preg_replace('/[^A-Za-z\ ]/', '', $row['text']));
-      $review .= ' ' . $text;
-    }
-
-    //push review to database here ********** this could be up to three reviews concat. together
-
-    $tokens = new TokensDocument(tokenize($review));
-    $docs[$id[0]] = $tokens;
-  }
-
-  return $docs;
-}
-
-//  $likes = the array returned from userInterests
-//  $tfidf = a tfidf object
-//  $docs = the array from getReviewsArray
-function createUserVector($likes, $tfidf, $docs) {
-  $userProfile = [];
-  foreach($likes as $like) {
-    array_push($userProfile, $tfidf->getTfIdf($docs['interests'], $like, 3));
-  }
-
-  return $userProfile;
-}
-
-//  $docs = the array from getReviewsArray
-//  $likes = the array returned from userInterests
-//  $tfidf = a tfidf object
-//  $userProfile = What is returned from create UserVector
-function poiRecommendArray($docs, $likes, $tfidf, $userProfile) {
-  $poisLiked = [];
-  foreach($docs as $key=>$poi) {
-    $vector = [];
-    foreach($likes as $like) {
-      array_push($vector, $tfidf->getTfIdf($poi, $like, 3));
-    }
-    $similarity = cosineSimilarity($userProfile, $vector);
-    if($similarity != -2) {
-      $poisLiked[$key] = $similarity;
-    }
-  }
-  asort($poisLiked);
-  array_pop($poisLiked);
-  $poisLiked = array_reverse($poisLiked, true);
-}
-
-
-
-
-//Functions above are for use later
-
-
-
+//Good resource on TFIDF
+//https://janav.wordpress.com/2013/10/27/tf-idf-and-cosine-similarity/
 
 function getRecommendations($link, $userID) {
 
@@ -109,63 +24,38 @@ function getRecommendations($link, $userID) {
     $likes = array();
     foreach($poiReviews as $poiReview) {      
       $review = explode(' ', strtolower(preg_replace('/[^A-Za-z\ ]/', '', $poiReview[0])));
-      array_shift($review);
-
-
-      $likes = array_merge($likes, $review);
+      $likes = array_filter(array_merge($likes, $review));
     }
-    //var_dump($likes);
+    // var_dump($likes);
 
     $docs = [];
     $tokens = new TokensDocument($likes);
     $docs['likes'] = $tokens;
-    //var_dump($docs['likes']);
     
 
 
     //
     // Get all review tokens in one document
-    //
+    // Build all review documents
     //
     $POI_reviews = $link->query('SELECT reviews, id FROM POIs')->fetch_all();
     $allReviews = array();
-    foreach($POI_reviews as $poiReview) {      
-      $review = explode(' ', strtolower(preg_replace('/[^A-Za-z\ ]/', '', $poiReview[0])));
-      array_shift($review);
-      
+    foreach($POI_reviews as $poiReview) {
+      if($poiReview[0] != Null) {     
+        $review = array_filter(explode(' ', strtolower(preg_replace('/[^A-Za-z\ ]/', '', $poiReview[0]))));
 
+        //Adds the new review onto the end of the array for one big array with all the words
+        $allReviews = array_merge($allReviews, $review);
 
-      $allReviews = array_merge($allReviews, $review);
-    }
-    
-    $allReviews = array_unique($allReviews);
-    $allReviewTokens = new TokensDocument($allReviews);
-    $docs['all'] = $allReviewTokens;
-    //var_dump($docs['all']);
-    //
-    // Get all review tokens in one document per poi
-    //
-    //
-
-
-
-    $POI_reviews = $link->query('SELECT reviews, id FROM POIs')->fetch_all();
-    //$poiDocs = array();
-    foreach ($POI_reviews as $review) {
-      //var_dump($review);
-      if($review[0] != Null) {
-        $tokens = new TokensDocument(tokenize($review[0]));
-        $docs[$review[1]] = $tokens;
+        //Adds a new vector for the current poi
+        $tokens = new TokensDocument($review);
+        $docs[$poiReview[1]] = $tokens;
       }
     }
+    //Makes a big array with one of every word from the reviews. - 2081 total words
+    $allReviews = array_unique($allReviews);
+    //var_dump($allReviews);
     //var_dump($docs);
-
-
-
-    //var_dump($likes);
-    //var_dump($poiDocs);
-    
-    //var_dump($docs['likes']);
 
 
 
@@ -177,116 +67,33 @@ function getRecommendations($link, $userID) {
     $userTfidf = new TfIdf($docsCollection);
 
 
-    //creates the user profile vector
-
-    $userProfile = [];
-    foreach($allReviews as $word) {
-      if($word != '') {
-        array_push($userProfile, $userTfidf->getTfIdf($docs['likes'], $word, 3));
+    //
+    // Creates all the vectors for the POIs and one for the user
+    //
+    $vectorCollection = [];
+    $vector = [];
+    foreach($docs as $key => $values) {
+      foreach($allReviews as $word) {
+        $vector[$word] = $userTfidf->getTfIdf($values, $word, 3);
       }
+      $vectorCollection[$key] = $vector;
     }
-    //var_dump($userProfile);
+    //var_dump($vectorCollection['likes']);
 
 
-    $POI_reviews = $link->query('SELECT reviews, id FROM POIs')->fetch_all();
     $poisLiked = [];
-    foreach($POI_reviews as $poiReview) { 
-      if($poiReview[0] != '') {
-        // $review = explode(' ', strtolower(preg_replace('/[^A-Za-z\ ]/', '', $poiReview[0])));
-        // //array_shift($review);
-        // $review = array_filter($review);
-        // $review
-        $vector = [];
-          //var_dump($token);
-        foreach($allReviews as $word) {
-          if($word != '') {
-            array_push($vector, $userTfidf->getTfIdf($docs[$poiReview[1]], $word, 3));
-          }
-        }
-                                              //review, each word in all
-        
-        // var_dump(count($vector));
-        // var_dump(count($userProfile));
-        
-        $similarity = cosineSimilarity($userProfile, $vector);
-        //var_dump($similarity);
-        if($similarity != -2) {
-          $poisLiked[$poiReview[1]] = $similarity;
-        }
-        
-      } 
+    foreach($vectorCollection as $key => $vector) {
+      $similarity = cosineSimilarity($vectorCollection['likes'], $vector);
+      $poisLiked[$key] = $similarity;
     }
+    unset($poisLiked['likes']);
     asort($poisLiked);
-    array_pop($poisLiked);
-    //var_dump($docs['all']);
-    // //Prints the recommended POIs
-    // foreach($poisLiked as $key=>$value) {
-    //   echo $key . '=' . $value . '<br>';
-    // }
+    $poisLiked = array_reverse($poisLiked, true);
+
+    //var_dump($poisLiked);
 
     //returns an ordered list of POIs to recommend
-    
-    
-    return json_encode(array_slice(array_reverse($poisLiked, true), 0, 5, true));
-    
-
-
-
-    
+    return json_encode(array_slice($poisLiked, 0, 5, true));
   }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// $response = $client->request('GET', 'https://api.yelp.com/v3/businesses/UFCN0bYdHroPKu6KV5CJqg/reviews?limit=20&sort_by=yelp_sort', [
-//   'headers' => [
-//     'Authorization' => 'Bearer FPHBQC5fbtVpUqt4lQtAmTPXNWzDKblHryRIRIfoL5PYHgLmW109muvBkAqYyscdeNerih_ZQrxs4WGnp-xf4pgyBDbEmO36NlUS8MB6GvgJp52qoqW_nUdvG9uOY3Yx',
-//     'accept' => 'application/json',
-//   ],
-// ]);
-
-// //echo $response->getBody();
-// //$data = $response->getBody();
-// $data = json_decode($response->getBody(), true);
-
-
-// $docs = [];
-// foreach($data["reviews"] as $row) {
-//   $text = strtolower(preg_replace('/[^A-Za-z0-9\- ]/', '', $row['text']));
-//   $tokens = new TokensDocument(tokenize($text));
-//   //$tokens->applyStemmer(new DictionaryStemmer(new EnchantAdapter(), new SnowballStemmer()));
-//   array_push($docs, $tokens);
-//   foreach($tokens->getDocumentData() as $token) {
-//     echo $token . '<br>';
-//   }
-// }
-// $docsCollection = new DocumentArrayCollection($docs);
-// $tfidf = new TfIdf($docsCollection);
-
-// //$chicken = $tfidf->getTfIdf($docs[1], 'Eagle', 3);
-// $chicken = $tfidf->getIdf();
-// foreach($chicken as $chick) {
-//   echo '<br>' . $chick;
-// }
-
-
-// foreach ($data["reviews"] as $row) {
-//   //get the POI details
-//   $review = $row['text'];
-//   echo '<br>' . $review;
-// }
-
 ?>
