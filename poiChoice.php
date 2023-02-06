313f4768d333464e8a716415768045063cdda463
Alec <alecjoshuagreen@gmail.com>
2023-02-06 16:04:46 -0500

    Styling for Map

--- poiChoice.php
+++ poiChoice.php
@@ -8,17 +8,30 @@
     //Also removes all markup within the div #choice
     function getPoiInfo($link, $ids, $pos, $likes) {
         $id = $ids[$pos];
-        $results = $link->query('SELECT name, image_url FROM pois WHERE API_ID = "' . $id . '";')->fetch_all();
-
+        $results = $link->query('SELECT name, image_url, Lat, Lng FROM pois WHERE API_ID = "' . $id . '";')->fetch_all();
+        $leafletLink = "'http://www.openstreetmap.org/copyright'";
         foreach ($results as $row) {
             $name = $row[0];
             $image_url = $row[1];
+            $Lat = $row[2];
+            $Lng = $row[3];
             echo '<div id="' . $id . '">
                     <h1 class=' . $id . '>' . $name . '</h1>
                     <form method="post">
                         <button type="submit" name="Like' . $pos . '" value="' . $likes . ' ' . $id . '">Like</button>
                         <button type="submit" name="Dislike' . $pos . '" value="' . $likes . '">Dislike</button>
-                        <img src="'. $image_url . '" alt="'. $image_url . '">
+                        <div id="choice_map"></div>
+                        <script>                  
+                        var map = L.map("choice_map").setView([' . $Lat . ', ' . $Lng . '], 13);
+                        L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+                            maxZoom: 25,
+                            attribution: "&copy; <a href= ' . $leafletLink . '>OpenStreetMap</a>"
+                        }).addTo(map);
+                        var marker = L.marker([' . $Lat . ', ' . $Lng . ']).addTo(map);
+                        </script>
+                        <style> 
+                        #choice_map { height: 350px; width: 300px; }
+                        </style>
                     </form>
                   </div>';
         }
@@ -33,6 +46,13 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="styles/style.css">
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.3/dist/leaflet.css"
+     integrity="sha256-kLaT2GOSpHechhsozzB+flnD+zUyjE2LlfWPgU04xyI="
+     crossorigin=""/>
+     <!-- Make sure you put this AFTER Leaflet's CSS -->
+ <script src="https://unpkg.com/leaflet@1.9.3/dist/leaflet.js"
+     integrity="sha256-WBkoXOwTeyKclOHuWtc+i2uENFpDZ9YPdf5Hf+D7ewM="
+     crossorigin=""></script>
     <title>POI Survey</title>
 </head>
 <body>
