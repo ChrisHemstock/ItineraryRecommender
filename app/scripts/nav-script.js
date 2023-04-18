eae4787df0053d2d80e1bf57db630e611940a064
ChrisHemstock <chris.hemstock@myemail.indwes.edu>
2023-04-18 10:38:43 -0400

    fixed nav over map

--- app/scripts/nav-script.js
+++ app/scripts/nav-script.js
@@ -1,4 +1,9 @@
 document.querySelector('.menu-toggle').addEventListener('click', function(){
-    document.querySelector(".nav").classList.toggle("mobile-nav");
+    let present = document.querySelector(".nav").classList.toggle("mobile-nav");
+    if(present) {
+        document.getElementById("map").style.zIndex = -1;
+    } else {
+        document.getElementById("map").style.zIndex = 0;
+    }
     document.querySelector('.menu-toggle').classList.toggle("is-active");
  })
